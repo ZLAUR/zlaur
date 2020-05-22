621ab7f202084b8444c68c87e645ca1796bf0e82
ZLAUR <52978632+VIDEOON@users.noreply.github.com>
2020-05-22 00:57:26 -0500

    12345678

---
+++ js/alerta.js
@@ -0,0 +1,8 @@
+Swal.fire({
+title:'<h6 class="perro"><br> QUÉDATE EN CASA</h6>',
+html:'<P class="rojo">Ayuda a frenar el coronavirus<br/><b>1 QUÉDATE</b> en casa lo máximo posible<br/><b>2 MANTÉN</b> el distanciamiento social<br/><b>3 LÁVATE</b> las manos con frecuencia<br/><b>4 TOSE</b> cubriéndote con el codo<br/><b>5 LLAMA</b> si tienes síntomas</P>',
+icon:"info",
+confirmButtonText:"Está bien bro !",
+footer:"DERECHOS RESERVADOS &copy 2020",
+stopKeydownPropagation:"true",
+});
